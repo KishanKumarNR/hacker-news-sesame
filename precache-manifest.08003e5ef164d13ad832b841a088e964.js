8478c3c3fc558806d96a33550099487134649c19
Kumar <dev.nrk@gmail.com>
2020-06-16 20:54:09 +0530

    Updates

---
+++ precache-manifest.08003e5ef164d13ad832b841a088e964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd77585bc38f7a071ab187bfec701837",
+    "url": "/hacker-news-sesame/index.html"
+  },
+  {
+    "revision": "55351eabb66bda59a4a6",
+    "url": "/hacker-news-sesame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2c5096ac447877a2e0a",
+    "url": "/hacker-news-sesame/static/js/2.b8dd7749.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-sesame/static/js/2.b8dd7749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55351eabb66bda59a4a6",
+    "url": "/hacker-news-sesame/static/js/main.350ec7e6.chunk.js"
+  },
+  {
+    "revision": "b8848cab7cc9c2f00228",
+    "url": "/hacker-news-sesame/static/js/runtime-main.e2c241dc.js"
+  }
+]);
